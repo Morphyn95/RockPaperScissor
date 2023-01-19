01712a183fade18383b719c942c5ddb4e40deeb8
David <david.ly@efashion-paris.com>
2023-01-19 13:34:26 +0100

    Change argument structure.

--- app.js
+++ app.js
@@ -5,17 +5,17 @@
 // document.getElementById("paper").onclick = battle(paper.text) // paper est une variable, or paper n'existe pas
 // document.getElementById("paper").onclick = battle(.paper) // warning de vscode. Mauvaise syntaxe. .paper c'est l'attribut d'un objet 
 
-document.getElementById("paper").onclick = function () {
-    battle("paper") // c'est un string
-}
-
-
 
 document.getElementById("rock").onclick = function () {
-    battle("rock") // c'est un string
+    battle({
+        text: "rock", // c'est un string
+        value: 0, // c'est un int
+    }) 
 }
 
-
+document.getElementById("paper").onclick = function () {
+    battle("paper") 
+}
 
 document.getElementById("scissor").onclick = function () {
     battle("scissor") // c'est un string
